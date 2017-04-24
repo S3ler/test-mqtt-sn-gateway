ae3aeab83431c1f4774ea53f242f9e6a33b1ba1b
Gabriel Nikol <gabriel-martin.nikol@stud.uni-bamberg.de>
2017-04-24 15:41:13 +0200

    added test classes for Publish, Sleeping, Will and WillUpdate

---
+++ src/functional/LinuxUdpGateway_Will.cpp
@@ -0,0 +1,19 @@
+//
+// Created by bele on 24.04.17.
+//
+
+#include <gtest/gtest.h>
+
+class LinuxUdpGateway_Will : public ::testing::Test {
+public:
+    virtual void TearDown() {
+
+    }
+
+    virtual void SetUp() {
+
+    }
+
+};
+
+// TODO implement tests
