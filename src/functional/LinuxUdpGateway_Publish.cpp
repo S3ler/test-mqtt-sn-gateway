60d764bbd0641a93dc9c27009dc9bb0e9e9d1007
Gabriel Nikol <gabriel-martin.nikol@stud.uni-bamberg.de>
2017-10-09 05:49:22 +0200

    cleaned test structure

--- src/functional/LinuxUdpGateway_Publish.cpp
+++ src/functional/LinuxUdpGateway_Publish.cpp
@@ -2,18 +2,4 @@
 // Created by bele on 24.04.17.
 //
 
-#include <gtest/gtest.h>
-
-class LinuxUdpGateway_Publish : public ::testing::Test {
-public:
-    virtual void TearDown() {
-
-    }
-
-    virtual void SetUp() {
-
-    }
-
-};
-
-// TODO implement tests
+// no implemented here see ../compliance/LinuxUdpGateway_Publish_Test.cpp
